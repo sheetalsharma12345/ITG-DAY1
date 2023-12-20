37578982db273139507390efb17db30de5314b93
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-12-20 16:05:39 +0530

    first commit

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,19 @@
+package mypkg;
+
+public class MyCalc {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		sum(2,3);
+		return a-b;
+	}
+	public static void main(String[] args) {
+		
+		MyCalc ob = new MyCalc();
+		System.out.println("Sum is "+ob.sum(30, 20));
+		System.out.println("Diff is "+ob.diff(30, 20));		
+	}
+}
