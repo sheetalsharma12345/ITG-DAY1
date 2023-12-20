69e2e25d976732a4a05ea584911b3fcd3af27b95
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-12-20 16:11:01 +0530

    mul added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -6,14 +6,19 @@ public class MyCalc {
 		return a+b;
 	}
 	public int diff(int a, int b)
-	{
-		sum(2,3);
+	{		
 		return a-b;
 	}
+	public int mul(int a, int b)
+	{		
+		return a*b;
+	}
+	
 	public static void main(String[] args) {
 		
 		MyCalc ob = new MyCalc();
 		System.out.println("Sum is "+ob.sum(30, 20));
-		System.out.println("Diff is "+ob.diff(30, 20));		
+		System.out.println("Diff is "+ob.diff(30, 20));	
+		System.out.println("Diff is "+ob.mul(30, 20));
 	}
 }
